0d3ce25c5c93f2c1a6fd3b22ab668472b46477a0
Tim Chase <git@tim.thechases.com>
2017-05-03 14:08:25 -0500

    Add support for symlinks

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # dedupe
-Deduplicate files within a given list of directories by keeping one copy and making the rest hard-links.
+Deduplicate files within a given list of directories by keeping one copy
+and making the rest hard-links.
+
+This should happen atomically on Linux/Unix platforms, but due to
+limitations in the Python/Windows layers, may not be atomic on Windows.
+
+Usage: dedupe.py [options] dir1 [dir2...]
+
+Options:
+  -h, --help            show this help message and exit
+  -n, --dry-run         Don't actually do the delete/link, just list what
+                        would be linked
+  -q, --quiet           Don't log which files were re-linked
+  -r, --recurse         Recurse into subdirectories
+  --min-size=MIN_SIZE   Minimum file-size to consider
+  -a ALGORITHM, --algorithm=ALGORITHM
+                        Choice of algorithm (one of DSA, DSA-SHA, MD4, MD5,
+                        RIPEMD160, SHA, SHA1, SHA224, SHA256, SHA384, SHA512,
+                        dsaEncryption, dsaWithSHA, ecdsa-with-SHA1, md4, md5,
+                        ripemd160, sha, sha1, sha224, sha256, sha384, sha512,
+                        whirlpool)
--- dedupe.py
+++ dedupe.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 from collections import namedtuple
 import hashlib
+import logging
 import optparse as op
 import os
 import sys
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("dedupe")
+
+SYMLINK_NEVER = "never"
+SYMLINK_FALLBACK = "fallback"
+SYMLINK_ALWAYS = "always"
+SYMLINK_DEFAULT = SYMLINK_NEVER
+SYMLINK_CHOICES = [
+    SYMLINK_NEVER,
+    SYMLINK_FALLBACK,
+    SYMLINK_ALWAYS,
+    ]
 
 FileInfo = namedtuple("FileInfo", [
     "name",
@@ -57,6 +71,17 @@ def build_parser():
         action="store",
         default=0,
         )
+    parser.add_option("-s", "--symlink", "--sym-link",
+        help="Should sym-links be used (%s)" % ", ".join(
+            ("[%s]" if sym == SYMLINK_DEFAULT else "%s") % sym
+            for sym
+            in SYMLINK_CHOICES
+            ),
+        choices=SYMLINK_CHOICES,
+        metavar="WHEN",
+        dest="symlink",
+        default=SYMLINK_DEFAULT,
+        )
     parser.add_option("-a", "--algorithm",
         help="Choice of algorithm (one of %s)" % (", ".join(algorithms)),
         choices=algorithms,
@@ -68,7 +93,7 @@ def build_parser():
 def find_dupes(options, *dirs):
     # {size: {dev: {hash: FileInfo}}}
     # {size: {dev: FileInfo}}
-    size_device_info_dict = {}
+    device_size_info_dict = {}
 
     if options.recurse:
         def walker(loc):
@@ -104,12 +129,15 @@ def find_dupes(options, *dirs):
                 this_device,
                 stat.st_ino,
                 )
-            device_fileinfo_dict = size_device_info_dict.setdefault(
-                file_size,
-                {},
-                )
-            if this_device in device_fileinfo_dict:
-                info_or_dict = device_fileinfo_dict[this_device]
+            if options.symlink == SYMLINK_NEVER:
+                size_fileinfo_dict = device_size_info_dict.setdefault(
+                    this_device,
+                    {},
+                    )
+            else:
+                size_fileinfo_dict = device_size_info_dict
+            if file_size in size_fileinfo_dict:
+                info_or_dict = size_fileinfo_dict[file_size]
                 if isinstance(info_or_dict, dict):
                     # we've already hashed files for this size+dev
                     hash_to_fileinfo = info_or_dict
@@ -131,7 +159,7 @@ def find_dupes(options, *dirs):
                             this_hash,
                             )
                     else:
-                        device_fileinfo_dict[this_device][this_hash] = this_fileinfo
+                        size_fileinfo_dict[file_size][this_hash] = this_fileinfo
 
                 else: # info_or_dict is just a FileInfo
                     file_info = info_or_dict
@@ -145,7 +173,7 @@ def find_dupes(options, *dirs):
                         file_info.name,
                         options.algorithm,
                         )
-                    device_fileinfo_dict[this_device] = {
+                    size_fileinfo_dict[file_size] = {
                         existing_file_hash: file_info,
                         }
                     if existing_file_hash == this_hash:
@@ -155,11 +183,11 @@ def find_dupes(options, *dirs):
                             this_hash,
                             )
                     else:
-                        device_fileinfo_dict[this_device][this_hash] = this_fileinfo
+                        size_fileinfo_dict[file_size][this_hash] = this_fileinfo
             else:
                 # we haven't seen this file size before
                 # so just note the full path for later
-                device_fileinfo_dict[this_device] =  this_fileinfo
+                size_fileinfo_dict[file_size] =  this_fileinfo
 
 def templink(source_path, dest_dir, name=None, prefix='tmp'):
      """Create a hard link to the given file with a unique name.
@@ -180,15 +208,26 @@ def templink(source_path, dest_dir, name=None, prefix='tmp'):
              break
      return dest_path
 
-def relink(patha, pathb, hash):
-    # because link() would fail with an EEXIST,
-    # we need to create a temp-name'd link file
-    # and then do an atomic rename() atop the original
+def symlink(patha, pathb, hash):
+    def relsymlink(a, b):
+        dest_loc = os.path.abspath(os.path.split(b)[0])
+        src_loc = os.path.relpath(a, dest_loc)
+        import pdb; pdb.set_trace()
+        os.symlink(src_loc, b)
+    return link(relsymlink, patha, pathb, hash)
+
+def hardlink(patha, pathb, hash):
+    return link(os.link, patha, pathb, hash)
+
+def link(linkfn, patha, pathb, hash):
+    # because link() can fail with an EEXIST,
+    # we create a temp-name'd link file and
+    # then do an atomic rename() atop the original
     # This could use tempfile.mktemp() but it has
     # been deprecated, so use the hash as a filename instead
     dest_path = os.path.split(pathb)[0]
     temp_name = os.path.join(dest_path, hash)
-    os.link(patha, temp_name)
+    linkfn(patha, temp_name)
     try:
         # this is documented as atomic
         os.rename(temp_name, pathb)
@@ -204,12 +243,26 @@ def relink(patha, pathb, hash):
 def dedupe(options, *dirs):
     for fileinfo_a, fileinfo_b, hash in find_dupes(options, *dirs):
         if not options.quiet:
-            print("%s -> %s" % (fileinfo_a.name, fileinfo_b.name))
+            log.info("%s -> %s", fileinfo_a.name, fileinfo_b.name)
         if not options.dry_run:
             try:
-                relink(fileinfo_a.name, fileinfo_b.name, hash)
+                if options.symlink == SYMLINK_ALWAYS:
+                    symlink(fileinfo_a.name, fileinfo_b.name, hash)
+                else:
+                    if fileinfo_a.dev == fileinfo_b.dev:
+                        # if they're on the same device
+                        # hardlink them
+                        hardlink(fileinfo_a.name, fileinfo_b.name, hash)
+                    elif options.symlink == SYMLINK_FALLBACK:
+                        # if the user wants to fallback to symlinks
+                        symlink(fileinfo_a.name, fileinfo_b.name, hash)
+                    else:
+                        log.warn("Could not hardlink %s to %s (different devices)",
+                            fileinfo_a.name,
+                            fileinfo_b.name,
+                            )
             except OSError:
-                sys.stderr.write("Could not relink %s to %s\n" % (
+                log.error("Could not link %s to %s\n" % (
                     fileinfo_a.name, fileinfo_b.name))
 
 def main():
